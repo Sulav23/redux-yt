b67e7f8f56dd0d29debf756888d15a8d7578dea3
Sulav23 <sulav.shrestha@xrdig.com>
2023-01-20 17:57:33 +0545

    initial commit

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPosts,
+  getPostsError,
+  getPostStatus,
+  selectAllPosts,
+} from "./postsSlice";
+import { useEffect } from "react";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostStatus);
+  const postsError = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>Loading...</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  }
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+export default PostsList;
